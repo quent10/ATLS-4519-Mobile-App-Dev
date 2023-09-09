7f1a948d03e75322647ae81323a429875b4d385e
quent10 <quwi1951@colorado.edu>
2023-09-09 15:44:44 -0600

    Update ContentView.swift
    This adds an online and offline to the app

--- ATLS 4519/ContentView.swift
+++ ATLS 4519/ContentView.swift
@@ -8,19 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var toggleIsOn: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            
+            Toggle(
+                isOn: $toggleIsOn,
+                label: {
+                    Text("Status")
+                    Text(toggleIsOn ? "Online" : "Offline")
+                    
+                })
+            .toggleStyle(SwitchToggleStyle(tint:Color.purple))
+            Spacer()
+            
+            Ellipse()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Ellipse()
+                .foregroundColor(.white)
+            Ellipse()
+                .foregroundColor(.red)
         }
-        .padding()
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
