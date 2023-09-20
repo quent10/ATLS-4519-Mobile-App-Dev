37e1ba1d129b7c8cbc43b423219d03d5db728bf6
quent10 <quwi1951@colorado.edu>
2023-09-20 16:56:11 -0600

    remade atlas 4519app

---
+++ ATLS 4519/ATLAS4519App.swift
@@ -0,0 +1,17 @@
+//
+//  ATLAS4519App.swift
+//  ATLS 4519
+//
+//  Created by Quentin Wingard on 9/20/23.
+//
+import SwiftUI
+
+
+@main
+struct ATLS_4519App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
